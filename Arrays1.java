580cc118f0931f08e67996f8549aa62412fa3cb6
Vinicius Oliveira <75057334+Skonks@users.noreply.github.com>
2021-02-09 12:32:36 -0300

    Add files via upload

---
+++ Arrays1.java
@@ -0,0 +1,16 @@
+
+public class Arrays1 {
+
+	public static void main(String args[]) {
+
+		int[] idades = new int[3]; // recomendado
+		// int idades2 []; não recomendado
+		idades[0] = 15;
+		idades[1] = 21;
+		idades[2] = 30;
+
+		System.out.println("Idade 1: " + idades[0]);
+		System.out.println("Idade 2: " + idades[1]);
+		System.out.println("Idade 3: " + idades[2]);
+	}
+}
